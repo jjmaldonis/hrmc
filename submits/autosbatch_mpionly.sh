dd40ef408ec37cc68cd1d5a626092c51a042eed0
refreshx2 <jjmaldonis@gmail.com>
2014-04-16 13:57:37 -0500

    fixed autosbatch_mpionly script

--- submits/autosbatch_mpionly.sh
+++ submits/autosbatch_mpionly.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-LINE1=$(sed -n "/--nodes=/=" slurm.sh)
-LINE2=$(sed -n "/--ntasks=/=" slurm.sh)
-
 # Submit jobs. Assume 16 cores per proc. Make sure line numbers are the same.
 for (( i = 1; i<= 16 ; i++ ))
 do
-    sed -i "/--nodes=/c #SBATCH --nodes=$i                      # number of nodes requested (n)" slurm.sh
-    sed -i "/--ntasks=/c #SBATCH --ntasks=$i                    # required number of CPUs (n)" slurm.sh
-    sbatch slurm.sh
+    sed -i "/--nodes=/c #SBATCH --nodes=$i                      # number of nodes requested (n)" submits/slurm_noomp.sh
+    sed -i "/--ntasks=/c #SBATCH --ntasks=$((16*$i))                    # required number of CPUs (n)" submits/slurm_noomp.sh
+    sbatch submits/slurm_noomp.sh
 done
 
--- submits/slurm_noomp.sh
+++ submits/slurm_noomp.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=7-00:00:00               # run time in days-hh:mm:ss
 
 #SBATCH --nodes=16                      # number of nodes requested (n)
-#SBATCH --ntasks=256                     # required number of CPUs (n)
+#SBATCH --ntasks=256                    # required number of CPUs (n)
 #SBATCH --ntasks-per-node=16             # default 16 (Set to 1 for OMP)
 #SBATCH --cpus-per-task=1              # default 1 (Set to 16 for OMP)
 ##SBATCH --mem=16384                    # total RAM in MB, max 64GB  per node
