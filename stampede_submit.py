17189804f4cc0c5116df33da827a55fd64fa89cf
refreshx2 <jjmaldonis@gmail.com>
2014-07-31 19:32:05 -0500

    really cleaned up the stampede submit file. can easily resume jobs now

--- stampede_submit.py
+++ stampede_submit.py
@@ -1,56 +1,61 @@
 import sys, os
 import shlex, subprocess, shutil
 
+def run_subproc(args):
+    """ Run subprocess given args as a command line string"""
+    print(args)
+    args = shlex.split(args)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pcomm = p.communicate()
+    poutput = pcomm[0] #p.stdout.read()
+    perr = pcomm[1] #p.stderr.read()
+    print(poutput,perr)
+    preturncode = p.wait()
+    if(preturncode != 0):
+        raise Exception("{0} failed!".format(args[0]))
+    return pcomm
+
+def submit_job(paramfile,prev_jobid,ss,modelfile=None):
+    """ Pass None to prev_jobid if using param_file.in as paramfile """
+    # Call sed to change starting step number to ss
+    args = "sed -i '5s/.*/{0}   !startingstep/' {1}".format(ss,paramfile)
+    pcomm = run_subproc(args)
+
+    # Call sed to change modelfile if modelfile was given
+    if(modelfile != None):
+        args = "sed -i '2s/.*/model_final_{0}.txt/' {1}".format(jobid,paramfile)
+        pcomm = run_subproc(args)
+
+    # Do the submitting
+    if(prev_jobid != None):
+        args = "sbatch --dependency=afterok:{0} submits/slurm_noomp.sh {1}".format(jobid,paramfile)
+    else:
+        args = "sbatch submits/slurm_noomp.sh {0}".format(paramfile)
+    pcomm = run_subproc(args)
+    jobid = int(pcomm[0].strip().split('\n').pop().split()[-1])
+    return jobid
+
 def main():
+    """ If you want to resume a job, all you have to do is change ss and starting_param_filename """
     ts = 24000000 # total number of steps in the simulation
     js = 400000 # number of steps per job
-    ss = 0 # starting step, this number gets incremented by js on each submit
+    ss = 0 # starting step, this number gets changed in the loop ~ incremented by js on each submit
     temp = 50 # starting temperature
     mm = 1.5 # starting maxmove
+    starting_param_filename = 'param_file.in'
 
-    for i in range(0,ts/js):
-        if(i > 0):
-            src = os.getcwd() + '/param_file.in'
-            dst = os.getcwd() + '/param_file.{0}.in'.format(jobid)
+    for s in range(ss, ts, js):
+        if(s > ss):
+            # Copy the starting parameter file to another filename which includes the step being started
+            src = os.getcwd() + '/' + starting_param_filename
+            dst = os.getcwd() + '/param_file.{0}.in'.format(s)
             shutil.copyfile(src,dst)
-            #shutil.copyfile('paramfile.in','paramfile.{0}.in'.format(jobid))
-            args = "sed -i '5s/.*/{0}   !startingstep/' param_file.{1}.in".format(ss,jobid)
-            args = "sed -i '2s/.*/model_final_{0}.txt/' param_file.{0}.in".format(jobid)
+            # Submit the job using that parameter file, but change it first
+            jobid = submit_job(dst,jobid,ss,'model_final_{0}.txt'.format(jobid))
         else:
-            args = "sed -i '5s/.*/{0}   !startingstep/' param_file.in".format(ss)
-        args = shlex.split(args)
-        print(args)
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        poutput = p.stdout.read()
-        perr = p.stderr.read()
-        print(poutput,perr)
-        preturncode = p.wait()
-
-        if(preturncode != 0):
-            raise Exception("sed failed!")
-        if(i > 0):
-            args = "sbatch --dependency=afterok:{0} submits/slurm_noomp.sh param_file.{0}.in".format(jobid)
-            #args = "qsub --dependency=afterok:{0} submits/slurm_noomp.sh param_file.{0}.in".format(jobid)
-        else:
-            args = "sbatch submits/slurm_noomp.sh param_file.in"
-            #args = "qsub submits/slurm_noomp.sh param_file.in"
-        args = shlex.split(args)
-        print(args)
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        pcomm = p.communicate()
-
-        print('')
-        print(pcomm[0].strip())
-        print(pcomm[1].strip())
-        #jobid = int(pcomm[0].strip().split('\n').pop().split()[2])
-        #print(jobid)
-        print('')
-
-        preturncode = p.wait()
-        if(preturncode != 0):
-            raise Exception("sbatch failed!")
-        jobid = int(pcomm[0].strip().split('\n').pop().split()[-1])
-        ss = ss + js
+            # On the first run, just submit the starting parameter file
+            dst = starting_param_filename
+            jobid = submit_job(dst,None,ss)
 
 
 if __name__ == '__main__':
