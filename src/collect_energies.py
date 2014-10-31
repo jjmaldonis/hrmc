5ea341994be76ba15c26af432fbb58c6a08f9152
refreshx2 <jjmaldonis@gmail.com>
2014-10-31 11:25:31 -0500

    added comments to collect energy python file. this is also in model analysis now, probably should use that version

--- src/collect_energies.py
+++ src/collect_energies.py
@@ -4,20 +4,26 @@ from model import Model
 from subproc import run_subproc
 
 def collect(m,modelfile):
+    # Get some paramfile with the correct format
     content = open('/home/jjmaldonis/development/mostupdated_hrmc_for_dev/hrmc/param_file.in').readlines()
     content[1] = modelfile+'\n'
+    # After changing the modelfile to the one that was given, save a temporary paramfile
     open('paramfile.temp.in','w').writelines(content)
     path = os.getcwd()
+    # Get the eam file if it isnt here already
     if( not os.path.isfile('ZrCuAl2011.eam.alloy') ):
         copy = True
         shutil.copyfile('/home/jjmaldonis/development/mostupdated_hrmc_for_dev/hrmc/ZrCuAl2011.eam.alloy',path+'/ZrCuAl2011.eam.alloy')
     else:
         copy = False
+    # Run the energy_per_atom code and keep the output
     output = run_subproc('/home/jjmaldonis/bin/energy_per_atom {0}/paramfile.temp.in'.format(path))
+    # Remove temporary paramfile and eam file if it wasnt here
     os.remove('paramfile.temp.in')
     if(copy):
         os.remove(path+'/ZrCuAl2011.eam.alloy')
     output = output.strip().split('\n')
+    # Collect and save the energies per atom into atom.energy
     while('Avg energy per atom' not in output[0]):
         output.pop(0)
     output.pop(0)
@@ -26,6 +32,7 @@ def collect(m,modelfile):
         at = int(line[0]) - 1
         energy = float(line[1])
         m.atoms[at].energy = energy
+    # Return the updated model, but it should be changed upon return anyways
     return m
 
 def main():
