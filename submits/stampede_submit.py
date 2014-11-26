5bb387d5961cb3c82d2fcee05f16044152ef334e
refreshx2 <jjmaldonis@gmail.com>
2014-11-26 09:58:04 -0600

    fixed bug in stampede submit file that incorrectly altered how often we decremented temp and max move

--- submits/stampede_submit.py
+++ submits/stampede_submit.py
@@ -16,11 +16,11 @@ def run_subproc(args):
         raise Exception("{0} failed!".format(args[0]))
     return pcomm
 
-def submit_job(paramfile, prev_jobid, s, js, modelfile):
+def submit_job(paramfile, prev_jobid, s, ds, modelfile):
     """ Pass None to prev_jobid if using param_file.in as paramfile """
     
     # Call sed to change starting step number to s
-    args = "sed -i '5s/.*/{0} {1}   !startingstep, how often we decrement temp and max move/' {2}".format(s,js,paramfile)
+    args = "sed -i '5s/.*/{0} {1}   !startingstep, how often we decrement temp and max move/' {2}".format(s,ds,paramfile)
     pcomm = run_subproc(args)
 
     # Call sed to change modelfile if modelfile was given
@@ -41,6 +41,7 @@ def main():
     """ If you want to resume a job, all you have to do is change ss """
     ts = 4000000 # total number of steps in the simulation
     js = 2000000 # number of steps per job
+    ds = 200000  # how often we decrement temp and max move
     ss = 0 # starting step, this number gets changed in the loop ~ incremented by js on each submit
     starting_param_filename = 'param_file.in' # It is expected that the starting parameters in here are correct
 
@@ -51,11 +52,11 @@ def main():
             dst = os.getcwd() + '/param_file.{0}.in'.format(s)
             shutil.copyfile(src,dst)
             # Submit the job using that parameter file, but change it first
-            jobid = submit_job(dst,jobid,s,js,'model_final_{0}.txt'.format(jobid))
+            jobid = submit_job(dst,jobid,s,ds,'model_final_{0}.txt'.format(jobid))
         else:
             # On the first run, just submit the starting parameter file
             dst = starting_param_filename
-            jobid = submit_job(dst,None,s,js,None)
+            jobid = submit_job(dst,None,s,ds,None)
         print("Jobid: {0}".format(jobid))
 
 
