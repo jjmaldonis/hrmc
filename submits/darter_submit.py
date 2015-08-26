66cb1401feea83b17718c14c678758471df058a2
Jason Maldonis <darter2.nics.utk.edu>
2015-08-26 12:18:15 -0400

    minor nice fix so i can pull and duplicate

--- submits/darter_submit.py
+++ submits/darter_submit.py
@@ -20,7 +20,7 @@ def submit_job(paramfile, prev_jobid, s, es):
     """ Pass None to prev_jobid if using param_file.in as paramfile """
     
     # Call sed to change starting step number to s
-    args = "sed -i '5s/.*/{0} {1}\t\t# starting step, ending step/' {2}".format(s,es,paramfile)
+    args = "sed -i '5s/.*/{0} {1}\t\t# starting step, ending step/' {2}".format(s, es, paramfile)
     pcomm = run_subproc(args)
 
     # Do the submitting
@@ -31,7 +31,7 @@ def submit_job(paramfile, prev_jobid, s, es):
         args = "qsub submits/pbs.sh"
     else:
         # Call sed to change modelfile based on previous job id
-        args = "sed -i '2s/.*/model_final_{0}.xyz/' {1}".format(prev_jobid,paramfile)
+        args = "sed -i '2s/.*/model_final_{0}.ocoee.nics.utk.edu.xyz/' {1}".format(prev_jobid,paramfile)
         pcomm = run_subproc(args)
         args = "sed -i '11s/.*/aprun \-n $PBS_NNODES .\/rmc $PBS_JOBID {0}/' {1}".format(paramfile.replace('/', '\/'), 'submits/pbs.sh')
         pcomm = run_subproc(args)
@@ -45,22 +45,22 @@ def submit_job(paramfile, prev_jobid, s, es):
 def main():
     """ If you want to resume a job, all you have to do is change ss """
     ts = 4000000 # total number of steps in the simulation
-    js = 2000000 # number of steps per job
+    js = 1500000 # number of steps per job
     ss = 0 # starting step, this number gets changed in the loop ~ incremented by js on each submit
-    starting_param_filename = 'param_file.in' # It is expected that the starting parameters in here are correct
+    starting_param_filename = 'param_resume.in' # It is expected that the starting parameters in here are correct
 
     for s in range(ss, ts, js):
         if(s > ss):
             # Copy the starting parameter file to another filename which includes the step being started
             src = os.getcwd() + '/' + starting_param_filename
             dst = os.getcwd() + '/param_file.{0}.in'.format(s)
-            shutil.copyfile(src,dst)
+            shutil.copyfile(src, dst)
             # Submit the job using that parameter file, but change it first
-            jobid = submit_job(dst,jobid,s,min(s+js,ts))
+            jobid = submit_job(dst, jobid, s, min(s+js, ts))
         else:
             # On the first run, just submit the starting parameter file
             dst = starting_param_filename
-            jobid = submit_job(dst,None,s,min(s+js,ts))
+            jobid = submit_job(dst, None, s, min(s+js, ts))
         print("Jobid: {0}".format(jobid))
 
 
